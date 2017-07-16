f69a03ad4b36ccbdae9f48022db2f6dc3f84b0ef
Wu Zefeng <flamesdesperado@gmail.com>
2017-07-16 16:26:19 +0800

    update

--- request.js
+++ request.js
@@ -1,9 +1,9 @@
 $('#send').on('click', function () {
-	console.log("Hi")
+    $('#result').val("Please wait for the response...")
 	$.ajax({
 		type: "POST",
 		url: "https://symptom-checker-api.herokuapp.com/api/symptom_checker",
-		data: JSON.stringify({"complaint": $("#complaint").val()}),
+		data: JSON.stringify({"complaint": $("#complaint").val(), "filter": $("#filter").val(), "mode": $("#mode").val()}),
 		success: function (data) {$('#result').val("Diagnoses: " + data.diagnoses + "\nDirection: " + data.direction)},
 		contentType: 'application/json'
 	});
